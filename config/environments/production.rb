644d107883d3a5ddda0b91a13fa6913794c06d2e
pantheraa <chatwithjai@gmail.com>
2016-10-05 16:52:10 +0530

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,7 +15,7 @@ Glowcon::Application.configure do
   config.action_controller.perform_caching = true
 
   # Don't care if the mailer can't send.
-  config.action_mailer.default_url_options = { :host => 'glownxt.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'https://glownxt.herokuapp.com' }
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
